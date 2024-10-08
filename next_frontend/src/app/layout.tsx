6243a664b4cb67131cfd159e7ef1442a87627192
Corban <50851004+xzCorban@users.noreply.github.com>
2024-10-08 18:00:48 +1300

    Added Navbar and layout files

--- next_frontend/src/app/layout.tsx
+++ next_frontend/src/app/layout.tsx
@@ -3,20 +3,20 @@ import './globals.css';
 import Link from 'next/link';
 
 export default function RootLayout({
-children,
+    children,
 }: {
-children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-return (
-<html lang="en">
-<body>
-<div className="header-nav">
-<Link href="/" className="header-button">Home</Link>
-<div>SPEED</div>
-<Link href="/form" className="header-button">New Article</Link>
-</div>
-<div className="container">{children}</div>
-</body>
-</html>
-);
+    return (
+        <html lang="en">
+            <body>
+                <div className="header-nav">
+                    <Link href="/" className="header-button">Home</Link>
+                    <div>SPEED</div>
+                    <Link href="/form" className="header-button">New Article</Link>
+                </div>
+                <div className="container">{children}</div>
+            </body>
+        </html>
+    );
 }
