41de6ae8e7df54706337e97889e78731666d435a
AusaafMohammed17 <mohammedausaaf17@gmail.com>
2024-10-20 17:08:23 +1300

    Implemented SearchArticle functionality

---
+++ next_frontend/src/app/book/search-book/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import SearchArticles from "@/components/SearchArticles";
+
+export default function SearchBookPage() {
+  return (
+    <main>
+      <SearchArticles />
+    </main>
+  );
+}
